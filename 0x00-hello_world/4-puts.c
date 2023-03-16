6393545ec66d1ba533c504d590c01c993db8832c
Med Dev <medterch@gmail.com>
2023-03-16 06:43:26 -0700

    Task 4 C program that prints exactly Programming is like building a multilingual puzzle commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 
 
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+int main()
+{      /* main */
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
