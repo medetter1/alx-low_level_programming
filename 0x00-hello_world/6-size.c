a09fbaf77ff81989a5acab30d629e417f75b9afb
Med Dev <medterch@gmail.com>
2023-03-16 09:19:50 -0700

    Task 6 C program that prints the size of various types on the computer it is compiled and run on commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,12 @@ int main(void)
 int a;
 float b;
 long int li;
-long long int lli;
+long long int d;
 char c;
 printf("Size of a char : %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an integer : %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a float : %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long integer : %lu byte(s)\n", (unsigned long)sizeof(li));
-printf("Size of a long long integer : %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf("Size of a long long integer: %lu byte(s)\n", (unsigned long)sizeof(d));
 return (0);
 }
