df78a1a9eb73a0abb103f4a072cd21eed0b98aa2
Med Dev <medterch@gmail.com>
2023-03-17 11:55:19 -0700

    Task 8  a program that prints all the numbers of base 16 in lowercase, followed by a new line commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
