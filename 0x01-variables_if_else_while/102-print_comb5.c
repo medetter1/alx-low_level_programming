8534c7310abca37e95cae386d0ff1c5d8f2e8274
Med Dev <medterch@gmail.com>
2023-03-17 18:15:39 -0700

    Task 12 a program that prints all possible combinations of two two-digit numbers commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99 ; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 98 && j == 99)
+			{
+				continue;
+			}
+				putchar(',');
+				putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
