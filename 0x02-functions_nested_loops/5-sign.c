e9972c226a775f37433211c66df7780faf334faf
Med Dev <medterch@gmail.com>
2023-03-21 13:07:39 -0700

    Task 5 a function that prints the sign of a number commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - function
+ * @n: single letter input
+ * Return: int
+ */
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
