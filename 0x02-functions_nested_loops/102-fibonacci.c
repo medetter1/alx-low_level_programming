dae255da74521df5a6ec10d59a989f95bf8ac632
Med Dev <medterch@gmail.com>
2023-03-21 19:08:57 -0700

    Task 14 a program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long count, i, j, k;
+
+	i = 0;
+	j = 1;
+	for (count = 0; count < 50; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu", k);
+		if (count == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
