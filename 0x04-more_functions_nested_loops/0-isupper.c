f3ba0c3a6eabca5e6d8fd3f9b1a1037a591b6493
Med Dev <medterch@gmail.com>
2023-03-23 11:17:24 -0700

    Task 0  a function that checks for uppercase character commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - function
+ * @c: single letter input
+ * Return: int
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
