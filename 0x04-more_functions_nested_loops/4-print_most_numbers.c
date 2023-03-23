c6542f7087f45b0f9768308e1527b3c2a0cb7029
Med Dev <medterch@gmail.com>
2023-03-23 14:55:40 -0700

    Task 4 a function that prints the numbers, from 0 to 9, followed by a new line except 2 and 4 commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers -  checks for checks for a digit (0 through 9).
+ * print_most_numbers -  checks for checks for a digit (0 through 9).
  *
- * Return: Always 0.
+ * Return: Always void.
  */
 void print_most_numbers(void)
 {
