308f383ac57666c2b5dba0ec559445cff113b729
Med Dev <medterch@gmail.com>
2023-03-27 16:25:42 -0700

    Task 4 a function that prints a string, in reverse, followed by a new line commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i, len, j;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+	j = len - 1;
+
+	while (s[j] >= 0)
+	{
+		_putchar(s[j]);
+		j--;
+	}
+
+	_putchar('\n');
+}
