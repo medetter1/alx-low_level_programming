fd731f0b5afd3bbffc4b8e1820eff18d3fb81d33
Med Dev <medterch@gmail.com>
2023-04-04 14:57:56 -0700

    Task 5 strstr function that locates a substring commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,34 +1,61 @@
 #include "main.h"
 /**
+ * lenght_str - function size
+ * @p: string
+ * Return: integer
+ *
+ */
+int lenght_str(char *p)
+{
+	int i;
+
+	i = 0;
+
+	while (p[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * _strstr - function
  * @haystack: first parameter
  * @needle: second parameter
  * Return: pointer
+ *
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int m, n, j, x, c;
+	char *p;
 
-	i = 0;
+	n = lenght_str(needle);
 	j = 0;
 
-	while (haystack[i] != '\0')
+	while (haystack[j] != '\0')
 	{
-		while (needle[j] != '\0')
+		if (haystack[j] == needle[0])
 		{
-			if (haystack[i] == needle[j])
+			p = &haystack[j];
+			m = j;
+			x = 0;
+
+			for (c = 0; c < n; c++)
 			{
-				do {
-					if (needle[j + 1] == '\0')
-					{
-						return (haystack);
-					}
-					i++;
-					j++;
-				} while (haystack[i] == needle[j]);
+				if (haystack[m] == needle[c])
+				{
+					x++;
+				}
+
+					m++;
 			}
-			haystack++;
 		}
+		if (x == n)
+		{
+			return (p);
+		}
+		j++;
 	}
 	return ('\0');
 }
