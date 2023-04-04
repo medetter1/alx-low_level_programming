8771eee9f94afe7c542e9ed3b68de498bba7ebc6
Med Dev <medterch@gmail.com>
2023-04-04 12:08:26 -0700

    Task 5 strstr function that locates a substring commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strstr - function
+ * @haystack: first parameter
+ * @needle: second parameter
+ * Return: pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (haystack[i] != '\0')
+	{
+		while (needle[j] != '\0')
+		{
+			if (haystack[i] == needle[j])
+			{
+				do {
+					if (needle[j + 1] == '\0')
+					{
+						return (haystack);
+					}
+					i++;
+					j++;
+				} while (haystack[i] == needle[j]);
+			}
+			haystack++;
+		}
+	}
+	return (0);
+}
+
