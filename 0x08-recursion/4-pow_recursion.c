8bac127128ab932268c11a0136cc41868cea3177
Med Dev <medterch@gmail.com>
2023-04-05 14:44:20 -0700

    Task 4 power of number recursion commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - pow function
+ * @x: base
+ * @y: exponent
+ *
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	
+		return (-1);
+	
+	if (y == 0)
+		return (1);
+	if (y > 0)
+	
+		--y;
+	return (x * _pow_recursion(x, y));
+	
+}
