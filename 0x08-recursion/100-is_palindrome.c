47f622258e688cb9b3e80282d36323cb1b71093b
Med Dev <medterch@gmail.com>
2023-04-06 11:18:29 -0700

    Task 7 advanced checkout palindrome string recursion commit

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _length_str - the size of string
+ * @st: string
+ * Retuen: integer
+ */
+int _length_str(char *st)
+{
+	if (*st == '\0')
+		return (0);
+	st++;
+	return (_length_str(st) + 1);
+}
+
+/**
+ * palindrome_rule - function check-out palindrome
+ * @p: string
+ * @i: first parameter for index 0
+ * @j: second parameter for last char of str
+ * Return: 1 is palindrome 0 is not
+ */
+int palindrome_rule(char *p, int i, int j)
+{
+	if (p[i] != p[j])
+		return (0);
+	if (i < j + 1)
+		return (palindrome_rule(p, i + 1, j - 1));
+	return (1);
+}
+/**
+ * is_palindrome - functin test
+ * @s: string to test
+ * Return: integer
+ */
+int is_palindrome(char *s)
+{
+	int le;
+
+	le = _length_str(s);
+	if (le == 0)
+		return (1);
+	return (palindrome_rule(s, 0, le - 1));
+}
