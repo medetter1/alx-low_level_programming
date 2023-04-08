50f3e96fbfed59cfc24308864b2a1016a8ecfd7b
Med Dev <medterch@gmail.com>
2023-04-08 16:30:38 -0700

    Task 1 argc argv a program that prints the number of arguments passed into it commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check code.
+ * @argc: argument counter.
+ * @argv: argument aray
+ *
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
