e4cdae3123c5e51116db3bd87a1ca736baa0501f
Med Dev <medterch@gmail.com>
2023-04-08 19:45:18 -0700

    Task 3 argc argv multiple of two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program multiplies two numbers.
+ * @argc: number of arguments
+ * @argv: array
+ *
+ * Return: always 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	int mult = 0;
+
+		if (argc < 3 || argc > 3)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mult);
+		return (0);
+
+}
