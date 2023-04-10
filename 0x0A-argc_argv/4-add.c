3575141ed6f0b3efb41254bc34752847bfc27bcd
Med Dev <medterch@gmail.com>
2023-04-09 17:47:57 -0700

    Task 4 program that adds two positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,17 +9,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, sum, j;
 
 	sum = 0;
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] < 48 || *argv[i] > 57)
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (argv[i][j] < 48 || argv[i][j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
 			sum += (atoi(argv[i]));
 		}
