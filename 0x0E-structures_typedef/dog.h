88783392f7a82effc40d6601158a65d46a4da9ae
Med Dev <medterch@gmail.com>
2023-04-17 16:13:18 -0700

    Task 0 typedef C

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,7 @@ struct dog
 
 typedef struct dog first_dog;
 
-void init_dog(struct dog *d, char *name, char *owner, float age);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 first_dog *new_dog(char *name, float age, char *owner);
 void free_dog(first_dog *d);
