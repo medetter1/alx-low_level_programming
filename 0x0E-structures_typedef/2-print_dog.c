51c7607830fd0ecf07b34b11e51bc6c8e50b7916
Med Dev <medterch@gmail.com>
2023-04-17 17:45:07 -0700

    Task 2 typedef C

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - display informations of dog
+ * @d: dog object
+ *
+ * Return: void
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if ((*d).name == NULL)
+			printf("(nil)\n");
+		else
+			printf("Name:%s\n", (*d).name);
+
+	printf("Age:%.5f\n", (*d).age);
+		if ((*d).owner == NULL)
+			printf("(nil)\n");
+		else
+			printf("Owner:%s\n", (*d).owner);
+	}
+}
