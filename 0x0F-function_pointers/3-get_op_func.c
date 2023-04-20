7f1c49159fbcf3235477de85f126901bb20c85ad
Med Dev <medterch@gmail.com>
2023-04-19 18:24:47 -0700

    Task 3 function pointer C

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * get_op_func - Selects the correct function to perform
  * @s: The operator passed as argument.
@@ -19,8 +20,11 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (i < 5)
+	{
+		if (!(strcmp(ops[i].op, s)))
+			return (ops[i].f);
 		i++;
-
-	return (ops[i].f);
+	}
+	return (NULL);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -63,10 +63,5 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
