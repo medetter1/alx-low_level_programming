fa402d2990257fbe889d0780903de11e88b8f182
Med Dev <medterch@gmail.com>
2023-04-19 17:46:01 -0700

    Task 3 function pointer C

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - function sum
+ * @a: integer
+ * @b: integer
+ * Return: integer res
+ */
+int op_add(int a, int b)
+{
+	int res = 0;
+
+	res = a + b;
+	return (res);
+}
+/**
+ * op_sub - function sub
+ * @a: integer
+ * @b: integer
+ * Return: integer res
+ */
+int op_sub(int a, int b)
+{
+	int res = 0;
+
+	res = a - b;
+	return (res);
+}
+/**
+ * op_mul - function multipliy
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_mul(int a, int b)
+{
+
+	return (a * b);
+}
+/**
+ * op_div - function division
+ * @a: first integer
+ * @b: second integer
+ * Return: integer res
+ */
+int op_div(int a, int b)
+{
+
+	return (a / b);
+}
+
+/**
+ * op_mod - function modulo
+ * @a: first parameter
+ * @b: second parameter
+ * Return: integer res
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
