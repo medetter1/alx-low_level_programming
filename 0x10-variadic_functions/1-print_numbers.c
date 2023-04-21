59dd7542dcd2a11b0695bf8bd50bec3ecb40801f
Med Dev <medterch@gmail.com>
2023-04-20 18:33:42 -0700

    Task 1 variadic function C

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,11 +16,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d ", va_arg(ap, unsigned int));
-		if (i != (n - 1) && separator != NULL)
-			printf("%s", separator);
+		printf("%d", va_arg(ap, int));
+
+			if (i != (n - 1) && separator != NULL)
+			{
+				printf("%s", separator);
+			}
 	}
 	va_end(ap);
-	putchar('\n');
+	printf("\n");
 
 }
