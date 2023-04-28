1791ffbadc9843bfd21eff17bf3745dd172fbc15
Med Dev <medterch@gmail.com>
2023-04-27 17:52:37 -0700

    linked list C task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,13 @@
 #ifndef LIST_H
 #define LIST_H
+/**
+ * struct list_s - singly linked list
+ * @s: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *s;
