0c81a0173fa061b397b133ca676f33d1a1ed5570
Med Dev <medterch@gmail.com>
2023-05-04 06:08:07 -0700

    Bit Manipulation Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,25 +7,26 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	int len, i;
-	int val = 1;
+	unsigned int result, base;
+	int i;
 
+	result = 0;
+	i = 0;
+	base = 1;
 	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	i = (len - 1);
-	for (; i >= 0; i--)
+	while (*(b + i))
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (*(b + i) != '0' && *(b + i) != '1')
 			return (0);
-
-		if (b[i] == '1')
-			result += val;
-		val *= 2;
+		i++;
+	}
+	for (i--; i >= 0; i--)
+	{
+		if (*(b + i) == '1')
+			result = result + base;
+		base = base * 2;
 	}
 
 	return (result);
